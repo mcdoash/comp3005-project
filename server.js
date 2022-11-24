d15f8f77e317e06242c02af3fed762fe25017308
mcdoash <mcdoash@users.noreply.github.com>
2022-11-24 12:00:04 -0500

    Connect to and test db

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ let books = require("./books.json");
 
 const express = require("express");
 let app = express();
-app.listen(3000);
-console.log("Server running on port 3000");
 
 const pug = require("pug");
 app.set("view engine", "pug");
@@ -11,6 +9,36 @@ app.set("views", "./views");
 
 app.use(express.json());
 app.use(express.static("public"));
+
+
+const { Client } = require("pg");
+let db = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "password",
+    database: "Project"
+});
+db.connect((e) => {
+    if(e) {
+        console.error("Cannot connect to the database", e.stack);
+    }
+    else {
+        console.log("Connection established");
+        app.listen(3000);
+        console.log("Server running on port 3000");
+        testDb();
+    }
+});
+
+function testDb() {
+    const query = "SELECT * FROM Publisher";
+    db.query(query, (err, res) => {
+        if(err) throw err;
+        console.log(res.rows);
+        db.end()
+    });
+}
 
 app.get("/", (req, res) => {
     res.status(200).render("index");
