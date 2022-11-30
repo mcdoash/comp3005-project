e8aa0704d53616d2097f4fe8acb555ec7371f4ab
mcdoash <mcdoash@users.noreply.github.com>
2022-11-30 12:38:02 -0500

    Get partial address and card data

--- db/order-queries.js
+++ db/order-queries.js
@@ -1,12 +1,10 @@
 const db = require("./");
 
-//not allss
-const getAddresses = "SELECT * FROM Address WHERE Address.Account = $1";
-const getCards = "SELECT * FROM Card WHERE Card.Account = $1";
+const getAddresses = "SELECT ID, Fname, Lname, Street FROM Address WHERE Address.Account = $1";
+const getCards = "SELECT Card_id, name, '************' || SUBSTRING(Card_num , 12, 16) AS Card_num FROM Card WHERE Card.Account = $1";
 
 exports.getAccountData = (email, callback) => {
     db.query(getAddresses, [email], (err, result) => {
-        console.log(result.rows);
         let res = { addresses: result.rows };
 
         db.query(getCards, [email], (err, result) => {
