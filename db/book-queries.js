07b1845a9c5c0980113dea28fabf9da370fce81d
mcdoash <mcdoash@users.noreply.github.com>
2022-12-04 22:48:20 -0500

    Partial autofill lookup

--- db/book-queries.js
+++ db/book-queries.js
@@ -16,13 +16,15 @@ const newBook = "INSERT INTO Book VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, DEF
 const checkBook = "SELECT COUNT(*) AS Exists FROM Book WHERE ISBN = $1;"
 
 
+//create a new book, return isbn
 exports.addBook = (data, callback) => {
     db.query(newBook, data, (err, result) => {
-        if(err) console.log(err);
+        if(err) callback(err);
         callback(err, result.rows[0].isbn);
     });
 }
 
+//add new genres for a specific book
 exports.addGenres = (book, genres, callback) => {
     let values = [];
     genres.forEach(genre => {
@@ -35,6 +37,7 @@ exports.addGenres = (book, genres, callback) => {
     });
 }
 
+//add new authors for a specific book
 exports.addAuthors = (book, authors, callback) => {
     let values = [];
     authors.forEach(author => {
@@ -48,6 +51,7 @@ exports.addAuthors = (book, authors, callback) => {
 }
 
 
+//get a list of the highest selling book info
 exports.getPopular = (callback) => {
     db.query(getTopBooks, (err, result) => {
         callback(err, result.rows);
@@ -55,12 +59,14 @@ exports.getPopular = (callback) => {
 }
 
 
+//check to see if a book with given isbn exists
 exports.checkBook = (isbn, callback) => {
     db.query(checkBook, [isbn], (err, result) => {
         callback(err, result.rows[0].exists);
     });
 }
 
+//retrieve a specific book by isbn
 exports.getSpecific = (isbn, callback) => {
     db.query(getSpecficBook, [isbn], (err, result) => {
         callback(err, result.rows[0]);
@@ -117,15 +123,23 @@ exports.checkStock = (books, callback) => {
 }
 
 
+
+/* match where a word starts with given string
+   ex. "fiction" gets both "Science Ficiton" and
+       "Speculative Fiction" but "ie" does not 
+       get "ScIEnce Fiction" */
+
+//get all matching author names
 exports.getAuthorMatch = (name, callback) => {
-    const authorMatch = "SELECT ARRAY_AGG(DISTINCT Author) Names FROM Authored WHERE LOWER(Author) LIKE LOWER('" + name + "%')";
+    const authorMatch = "SELECT ARRAY_AGG(DISTINCT Author) Names FROM Authored WHERE Author ~* '\\m(" + name + ")';";
 
     db.query(authorMatch, (err, result) => {
         callback(err, result.rows[0].names);
     });
 }
+//get all matching genre names
 exports.getGenreMatch = (name, callback) => {
-    const genreMatch = "SELECT ARRAY_AGG(DISTINCT Name) Names FROM Genre WHERE LOWER(Name) LIKE LOWER('" + name + "%')";
+    const genreMatch = "SELECT ARRAY_AGG(DISTINCT Name) Names FROM Genre WHERE Name ~* '\\m(" + name + ")';";
 
     db.query(genreMatch, (err, result) => {
         callback(err, result.rows[0].names);
--- db/publisher-queries.js
+++ db/publisher-queries.js
@@ -4,21 +4,24 @@ const checkPub = "SELECT COUNT(*) AS Exists FROM Publisher WHERE Name = $1;";
 
 const newPub = "INSERT INTO Publisher VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);";
 
+
+//create a new publisher
 exports.addPub = (data, callback) => {
     db.query(newPub, data, (err) => {
         callback(err);
     });
 }
 
-//get array of all publisher names that start with a specific string
+//get array of all publisher names where a word in name starts with a specific string
 exports.getPubMatch = (name, callback) => {
-    const pubMatch = "SELECT ARRAY_AGG(Name) Names FROM Publisher WHERE LOWER(Name) LIKE LOWER('" + name + "%')";
+    const pubMatch = "SELECT ARRAY_AGG(Name) Names FROM Publisher WHERE Name ~* '\\m(" + name + ")'";
 
     db.query(pubMatch, (err, result) => {
         callback(err, result.rows[0].names);
     });
 }
 
+//check if a publisher with a given name exists
 exports.checkPub = (name, callback) => {
     db.query(checkPub, [name], (err, result) => {
         callback(err, result.rows[0].exists);
