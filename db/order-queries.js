dc84230f73a2c0611d763dd3e2f22bfe4c449037
mcdoash <mcdoash@users.noreply.github.com>
2022-12-07 13:35:26 -0500

    View order

--- db/order-queries.js
+++ db/order-queries.js
@@ -5,7 +5,9 @@ const getCards = "SELECT Card_id, name, '************' || SUBSTRING(Card_num , 1
 
 const createOrder = "INSERT INTO Book_order VALUES(DEFAULT, $1, $2, $3, $4, DEFAULT, NULL, DEFAULT, NULL, NULL) RETURNING Number;";
 
-const getOrder = "";
+const getOrderData = "SELECT Book_order.Number, Book_order.total, Book_order.Order_date, Book_order.Tracking, Book_order.Cur_location, Book_order.Expected_date, Book_order.Arrival_date, Address.Fname, Address.Lname, Address.Street, Address.City, Address.Province, Address.Postal_code, Address.Country, Address.Phone_num, '************' || SUBSTRING(Card_num , 12, 16) AS Card_num FROM Book_order JOIN Card ON Book_order.Billing = Card.Card_id JOIN Address ON Book_order.Ship_address = Address.ID WHERE Book_order.Number = $1;";
+
+const getOrderBooks = "SELECT Book.ISBN, Book.Title, Book.Price, Sale.Quantity FROM Sale JOIN Book_order ON Sale.Order_num = Book_order.Number JOIN Book ON Sale.Book = Book.ISBN WHERE Book_order.Number = $1;";
 
 
 exports.getAccountData = (email, callback) => {
@@ -48,8 +50,18 @@ exports.createSales = (order, cart, callback) => {
 }
 
 exports.getOrder = (num, callback) => {
-    cart.forEach(item => {
-        
-    });
+    let orderData = {};
 
+    db.query(getOrderData, [num], (err, result) => {
+        if(err) callback(err);
+        else {
+            orderData.data = result.rows[0];
+            
+            db.query(getOrderBooks, [num], (err, result) => {
+                orderData.books = result.rows;
+                callback(err, orderData);
+            });
+        }
+    });
+    
 }
--- routes/order-router.js
+++ routes/order-router.js
@@ -3,27 +3,46 @@ let router = express.Router();
 const db = require("../db/order-queries");
 const bookDb = require("../db/book-queries");
 
-router.post("/", createOrder, createSales, showOrder);
+router.post("/", createOrder, createSales);
+router.get("/:order", showOrder);
 
 function createOrder(req, res, next) { 
     db.createOrder(req.session.user.email, req.session.cart, (err, num) => {
         if(err) console.error(err.stack);
-        console.log(num);
-        res.order = num;
+        res.orderNum = num;
         next();
     });
 }
 
-function createSales(req, res, next) {
-    db.createSales(res.order, req.session.cart.books, (err) => {
+function createSales(req, res) {
+    db.createSales(res.orderNum, req.session.cart.books, (err) => {
         if(err) console.error(err.stack);
-        next();
+        
+        res.statusCode = 204;
+        res.redirect("/orders/" + res.orderNum);
+        return;
     });
 }
+
+
+router.param("order" , (req, res, next, num) => {
+    db.getOrder(num, (err, order) => {
+        if(err) console.error(err.stack);
+        if(!order.data) {
+            res.status(404).send({error: "Order  " + num + " does not exist"});
+            return;
+        }
+        res.order = order;
+        res.orderNum = num;
+        next();
+    });
+});
+
 
 function showOrder(req, res) {
-    return;
+    res.status(200).render("order", {
+        order: res.order
+    });
 }
-
 
 module.exports = router;
