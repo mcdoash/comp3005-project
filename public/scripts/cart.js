1c4ba048b867995ac2716ebe7cb58ed93f174a41
mcdoash <mcdoash@users.noreply.github.com>
2022-12-09 15:43:02 -0500

    Fix cart quantity change input

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -32,6 +32,8 @@ function removeFromCart(isbn) {
 }
 
 function changeQuantity(isbn, input) {
+    const val = parseInt(input.value);
+    
     let req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 204) {
@@ -39,6 +41,9 @@ function changeQuantity(isbn, input) {
         }
     }
     req.open("PUT", "/cart/" + isbn);
-	req.setRequestHeader("Content-Type", "application/json");
-	req.send(JSON.stringify({quantity: input.value}));
+    req.setRequestHeader("Content-Type", "application/json");
+
+    if(!isNaN(val) && val > 0) { //don't send if invalid
+        req.send(JSON.stringify({quantity: val}));
+    }
 }
